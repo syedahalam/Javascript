f23808ad1711c9232f7ef24e83dd11a64ef905f3
syedahalam <syedahalam@yahoo.com>
2022-01-25 17:46:28 -0500

    challenge 1

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-let js = 'amazing';
-console.log(js);
-if (js === 'amazing') alert('JS is fun');
+// let js = 'amazing';
+// console.log(js);
+// if (js === 'amazing') alert('JS is fun');
+
+// Challenge 1
+// const markWeight = 78;
+// const johnWeight = 92;
+
+// const markHeight = 1.69;
+// const johnHeight = 1.95;
+
+// const mBmi = 78 / 1.69 ** 2;
+// const jBmi = 92 / 1.95 ** 2;
+
+// const isMarkHigherBMI = mBmi > jBmi;
+// console.log(isMarkHigherBMI);
+
+// ///////////////////////
+
+const markWeight = 95;
+const johnWeight = 92;
+
+const markHeight = 1.88;
+const johnHeight = 1.95;
+
+const mBmi = 95 / 1.88 ** 2;
+const jBmi = 92 / 1.95 ** 2;
+
+const markHigherBMI = mBmi > jBmi;
+console.log(markHigherBMI);
