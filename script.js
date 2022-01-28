5c21dc0103d63f4f921566e847fdc29d7eb7a577
syedahalam <syedahalam@yahoo.com>
2022-01-27 21:49:17 -0500

    challenge 3 completed objects

--- script.js
+++ script.js
@@ -324,3 +324,45 @@ const person = {
 // console.log(person.getSummary());
 
 ////////////////////completed/////////////
+
+///////////////Challenge 3///////////////////
+
+const mark = {
+	fullName: 'Mark Miller',
+	mass: 78,
+	height: 1.69,
+
+	calcBMI: function () {
+		return this.mass / this.height ** 2;
+	},
+};
+console.log(mark.calcBMI());
+
+const john = {
+	fullName: 'John Smith',
+	mass: 92,
+	height: 1.95,
+
+	calcBMI: function () {
+		return this.mass / this.height ** 2;
+	},
+};
+console.log(john.calcBMI());
+
+// console.log(`${mark.calcBMI()`}>`${john.calcBMI()}`?${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})`:`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})`;
+
+if (`${mark.calcBMI()}` > `${john.calcBMI()}`) {
+	console.log(
+		`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${
+			john.fullName
+		}'s (${john.calcBMI()})`
+	);
+} else {
+	console.log(
+		`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${
+			mark.fullName
+		}'s (${mark.calcBMI()})`
+	);
+}
+
+///////////////////completed////////////////
