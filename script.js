89f8ca4e32b7c4614e5d582ff043fd719a6c166a
syedahalam <syedahalam@yahoo.com>
2022-02-28 13:50:02 -0500

    section 1, coding challenge#1 done

--- script.js
+++ script.js
@@ -512,3 +512,22 @@ const person = {
 // console.log(calcAverage([2, 3, 6]));
 // console.log(calcAverage(tips));
 // console.log(calcAverage(totals));
+// feb 28, 2022- section - coding challenge # 1
+
+// const markMass = 78;
+// const markHeight = 1.69;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const markHeight = 1.88;
+
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+console.log(markBMI, johnBMI);
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
