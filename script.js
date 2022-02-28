857a0b413bd420f05da1e5107dccb5622157b34e
syedahalam <syedahalam@yahoo.com>
2022-02-28 16:13:42 -0500

    section1 coding challenge 2 completed

--- script.js
+++ script.js
@@ -514,20 +514,28 @@ const person = {
 // console.log(calcAverage(totals));
 // feb 28, 2022- section - coding challenge # 1
 
-// const markMass = 78;
-// const markHeight = 1.69;
+const markMass = 78;
+const markHeight = 1.69;
 
-// const johnMass = 92;
-// const johnHeight = 1.95;
+const johnMass = 92;
+const johnHeight = 1.95;
 
-const markMass = 95;
-const markHeight = 1.88;
+// const markMass = 95;
+// const markHeight = 1.88;
 
-const johnMass = 85;
-const johnHeight = 1.76;
+// const johnMass = 85;
+// const johnHeight = 1.76;
 
 const markBMI = markMass / markHeight ** 2;
 const johnBMI = johnMass / johnHeight ** 2;
-console.log(markBMI, johnBMI);
-const markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+// console.log(markBMI, johnBMI);
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+
+// section 1 - coding challenge # 2
+
+if (markBMI > johnBMI) {
+	console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
+} else {
+	console.log(`John's ${johnBMI} BMI is higher than Mark's ${markBMI}!`);
+}
