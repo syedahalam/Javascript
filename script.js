5c6b852d53c082bf25f9d5dd83b665bcf1d3aa65
syedahalam <syedahalam@yahoo.com>
2022-03-02 02:06:54 -0500

    code challenge objects done

--- script.js
+++ script.js
@@ -876,3 +876,40 @@
 // console.log(humna.age);
 
 // console.log(`${humna.firstName} is a ${calcAge()}`);
+
+///////// coding challenge # 3 Objects ////////////////
+
+// const mark = {
+// 	fullName: 'Mark Miller',
+// 	mass: 78,
+// 	height: 1.69,
+
+// 	calcBMI: function () {
+// 		this.bmi = this.mass / this.height ** 2;
+// 		return this.bmi;
+// 	},
+// };
+
+// const john = {
+// 	fullName: 'John Smith',
+// 	mass: 92,
+// 	height: 1.95,
+
+// 	calcBMI: function () {
+// 		this.bmi = this.mass / this.height ** 2;
+// 		return this.bmi;
+// 	},
+// };
+// mark.calcBMI();
+// john.calcBMI();
+// console.log(mark.bmi, john.bmi);
+
+// console.log(
+// 	`${
+// 		mark.bmi > john.bmi
+// 			? `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+// 			: `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})!`
+// 	}`
+// );
+
+///////////////loops///////////
