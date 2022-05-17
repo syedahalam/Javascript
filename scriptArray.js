09701766203120731b946b11a862107c558dcd47
Syeda Alam <syedahalam@yahoo.com>
2022-05-17 07:49:57 -0400

    arrays

--- scriptArray.js
+++ scriptArray.js
@@ -1,64 +1,30 @@
 'use strict';
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
+//  /challenge 1
+/* Test Data 1: Julia's data[3,5,2,12,7]
+				Kate's Data[4,1,15,8,3]
 
-// Data
-const account1 = {
-	owner: 'Jonas Schmedtmann',
-	movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-	interestRate: 1.2, // %
-	pin: 1111,
-};
+	Test Data 2: Julia's data[9,16,6,8,3]
+				Kate's Data[10,5,6,1,4]
 
-const account2 = {
-	owner: 'Jessica Davis',
-	movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-	interestRate: 1.5,
-	pin: 2222,
-};
+	const juliaDogs=[3,5,2,12,7]
+	const copyJulia=juliaDogs.slice(3)
+	
 
-const account3 = {
-	owner: 'Steven Thomas Williams',
-	movements: [200, -200, 340, -300, -20, 50, 400, -460],
-	interestRate: 0.7,
-	pin: 3333,
-};
+	const kateDogs=[4,1,15,8,3]
 
-const account4 = {
-	owner: 'Sarah Smith',
-	movements: [430, 1000, 700, 50, 90],
-	interestRate: 1,
-	pin: 4444,
-};
+const checkDogs=(arr1, arr2)=>{
 
-const accounts = [account1, account2, account3, account4];
+}
+checkDogs(dogs1, dogs2)*/
 
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+// const juliaDogs = [3, 5, 2, 12, 7];
+// const copyJulia1 = juliaDogs.splice(1);
+// console.log(copyJulia1);
+// const copyJulia2 = copyJulia1.splice(2, 3);
+// console.log(copyJulia2);
+// console.log(juliaDogs);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
